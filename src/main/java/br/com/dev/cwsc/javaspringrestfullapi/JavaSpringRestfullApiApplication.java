951162d5c8b5e57b51e5779624400037d79aabea
Carlos Camargos <cwscamargos@gmail.com>
2023-05-25 12:10:14 -0300

    Initial commit

---
+++ src/main/java/br/com/dev/cwsc/javaspringrestfullapi/JavaSpringRestfullApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dev.cwsc.javaspringrestfullapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringRestfullApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringRestfullApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/dev/cwsc/javaspringrestfullapi/JavaSpringRestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.cwsc.javaspringrestfullapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringRestfullApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
